83b26b76984c281e849509bb54b8b6ff45480a18
rayraxx <julia.rayra12@gmail.com>
2025-10-05 22:38:28 -0300

    primeiro commit

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function menuShow() {
+  console.log("clicou no menu"); // pra testar se o clique ta indo
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+    menuMobile.classList.remove('open');
+    document.querySelector('.icon').src = "assets/img/menu_white_36dp.svg";
+  } else {
+    menuMobile.classList.add('open');
+    document.querySelector('.icon').src = "assets/img/close_white_36dp.svg";
+  }
+}
